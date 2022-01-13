efa84b293c99c77c07657763d8758c60e3653663
Michael Hebblethwaite <32097294+michebble@users.noreply.github.com>
2022-01-13 14:44:26 +0900

    Add translation as img alt text

--- src/lib/SelectedItem.svelte
+++ src/lib/SelectedItem.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+  import { _ } from "svelte-i18n";
   export let name: string;
   export let amount: number;
   export let src: string;
 </script>
 
 <div>
-  <img {src} alt={name} />
+  <img {src} alt={$_(`items.${name}`)} />
   <p>{amount}</p>
 </div>
 
